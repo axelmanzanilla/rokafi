cdde3a4fc0e9867746fa1cbb271e6fe3416f03d7
Axel Manzanilla <axelmanzanilla@gmail.com>
2023-01-21 11:52:53 -0600

    changes in home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,24 @@
 import { Main } from "./HomeStyles";
 import Recommended from "../../components/Recommended/Recommended";
 import Categories from "../../components/Categories/Categories";
+import React, { useEffect, useState } from "react";
 
 function Home() {
+    let [products, setProducts] = useState([]);
+
+    function getData(){
+        let data = fetch("https://firestore.googleapis.com/v1beta1/projects/rokafi/databases/(default)/documents/products/");
+        products = data;
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    useEffect(() => {
+        getData();
+    }, products);
+
     return (
         <Main>
             <Recommended title="Lo más nuevo"/>
